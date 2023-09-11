0b1570a019295d30f86e3c83dffab4c3be5d83a4
Sabareesh Nikhil Chinta <csabareeshnikhil@gmail.com>
2023-09-10 18:53:57 -0700

    removed comments from setup.sql

--- Code/setup.sql
+++ Code/setup.sql
@@ -1,16 +1,12 @@
-drop table ev, all_ev, us_ev, non_us_ev;
+drop table if exists ev, all_ev, us_ev, non_us_ev;
 
 create table ev (county varchar(50) NULL, city varchar(50) NULL, state_code varchar(2) NULL, model_year integer NULL, make varchar(50) NULL, ev_type varchar(100) NULL, cafv varchar(100) NULL, range_miles integer NULL, utility varchar(200) NULL);
 
 \COPY ev FROM 'Data/ev_population.csv' DELIMITER ',' HEADER CSV;
 
-## For training + testing data: 
-
 create table all_ev (county varchar(50) NULL, city varchar(50) NULL, state_code varchar(2) NULL, model_year integer NULL, make varchar(50) NULL, ev_type varchar(100) NULL, cafv varchar(100) NULL, range_miles integer NULL, utility varchar(200) NULL, us_or_non_us varchar(50) NULL);
 
 \COPY all_ev FROM 'Data/ev_population_total.csv' DELIMITER ',' HEADER CSV;
-
-## Creating US and Non-US EV tables.
 
 create table non_us_ev as select * from all_ev where us_or_non_us ='Rest of the World';
 alter table non_us_ev drop us_or_non_us;
